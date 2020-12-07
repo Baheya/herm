14fd73fa883eadb73a61156807e1c54fe879625e
Baheya Khalifa <khalifa.baheya@gmail.com>
2020-12-07 12:56:31 +0400

    re-adding app folder

---
+++ components/Main.js
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+function Main({ children }) {
+  return (
+    <Box paddingLeft="40px" paddingTop="40px">
+      {children}
+    </Box>
+  );
+}
+export default Main;
