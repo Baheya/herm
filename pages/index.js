d8a723d0cc9d91606b259cfedb4f70b07793b542
Baheya Khalifa <khalifa.baheya@gmail.com>
2020-12-08 17:05:09 +0400

    fetching and displaying user data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import { Text } from '@chakra-ui/react';
+import withApollo from 'lib/apollo';
+import Account from '../components/account';
 import Layout from '../components/Layout';
 
-function Index() {
+function Index({ me }) {
   return (
-    <Layout>
+    <Layout me={me}>
       <Text fontSize="40px" color="brand.500" as="h1">
-        Hello, Herm!
+        Hello, {me.name}!
       </Text>
+      <Account />
     </Layout>
   );
 }
-export default Index;
+
+Index.getInitialProps = async function (context) {
+  const res = await fetch(`${process.env.BASE_URL}/api/me`, {
+    headers: {
+      cookie: context.req.headers.cookie,
+    },
+  });
+  const me = await res.json();
+
+  if (me.error) {
+    context.res.writeHead(302, {
+      Location: '/api/login',
+    });
+    context.res.end();
+    return;
+  }
+  return { me };
+};
+
+export default withApollo(Index);
